cd6cc394e2e24bc78f1c217bf47f931e4f273edc
rocioimpa <rocio.impaglione@gmail.com>
2019-10-26 22:34:29 -0300

    Adding items removal feature

--- src/js/components/menuItem.js
+++ src/js/components/menuItem.js
@@ -1,12 +1,13 @@
-import { addClass, article, button, div, h3, p, span, text } from '../builders';
+import { addClass, article, button, div, h3, p, span, text, i } from '../builders';
 import { formatPrice } from '../helpers';
 
 export default function menuItem(itemData = {}) {
   const name = addClass(h3(text(itemData.name)), 'name');
+  const remove = addClass(i(),'fa','fa-trash','hide','remove')
   const price = addClass(span(text(`$${formatPrice(itemData.price)}`)), 'price', 'is-pulled-right');
   const description = addClass(p(text(itemData.description)), 'desc');
 
-  const mediaContent = addClass(div(name, price, description), 'media-content');
+  const mediaContent = addClass(div(name, price, description,remove), 'media-content');
 
   const item = addClass(article(mediaContent), 'media', 'menu_item');
   item.dataset.key = itemData.id;
