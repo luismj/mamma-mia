7b182124b12bc47993582a3ecc89fd0b5ffdd996
rocioimpa <rocio.impaglione@gmail.com>
2019-10-28 23:11:04 -0300

    Cleanup

--- mamma-mia-express/public/js/components/addDishListener.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  
-  $('.addDish').click(function(){
-    var itemData = {name:"Dish name example", description:"Dish description example", price:5 }
-    var item = createMenuItem(itemData);
-    event.target.parentNode.insertBefore(item, event.target);
-  });
-
-})
--- mamma-mia-express/public/js/menuItem.js
+++
@@ -1,9 +0,0 @@
-function createMenuItem(itemData){
-    const name = addClass(h3(text(itemData.name)), 'name');
-    const price = addClass(span(text(`$${formatPrice(itemData.price)}`)), 'price', 'is-pulled-right');
-    const description = addClass(p(text(itemData.description)), 'desc');
-    const mediaContent = addClass(div(name, price, description), 'media-content');
-    const item = addClass(article(mediaContent), 'media', 'menu_item');
-    item.dataset.key = itemData.id;
-    return item;
-}
