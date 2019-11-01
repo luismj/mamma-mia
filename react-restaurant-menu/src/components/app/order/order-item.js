5cca88b93560912ef1bf06a20f1eedca98c544b4
Agustina Barbetta <abarbetta@medallia.com>
2019-10-31 23:26:33 -0300

    Cleanup unused import

--- react-restaurant-menu/src/components/app/order/order-item.js
+++ react-restaurant-menu/src/components/app/order/order-item.js
@@ -2,7 +2,6 @@ import React, { Component } from "react"
 import Card from "@material-ui/core/Card"
 import CardActionArea from "@material-ui/core/CardActionArea"
 import CardContent from "@material-ui/core/CardContent"
-import CardMedia from "@material-ui/core/CardMedia"
 import Typography from "@material-ui/core/Typography"
 
 export default class OrderItem extends Component {
