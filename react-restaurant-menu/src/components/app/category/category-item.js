913849128def9230475332d78c4418b9f986d6b2
rocioimpa <rocio.impaglione@gmail.com>
2019-11-03 03:18:36 -0300

    Added action buttons on categories cards (actions are not working yet)

--- react-restaurant-menu/src/components/app/category/category-item.js
+++ react-restaurant-menu/src/components/app/category/category-item.js
@@ -1,31 +1,42 @@
 import React, { Component } from "react"
 import Card from "@material-ui/core/Card"
-import CardActionArea from "@material-ui/core/CardActionArea"
 import CardContent from "@material-ui/core/CardContent"
 import Typography from "@material-ui/core/Typography"
+import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 
 export default class CategoryItem extends Component {
-  handleClick = () => { }
+//  handleClick = () => { }
+  handleRemoval = () => {
+
+  }
 
   render() {
     return (
       <Card
         className={`${this.props.className} ${
-          this.props.selected ? "-selected" : ""
-        }`}
+          this.props.selected ? "-selected" : ""}`}
       >
-        <CardActionArea onClick={this.handleClick}>
           <CardContent>
             <Typography
               gutterBottom
               variant="h6"
               component="h2"
-              className="item-title"
+              className="item-title inline"
             >
               {this.props.categoryTitle}
             </Typography>
           </CardContent>
-        </CardActionArea>
+          <CardActions>
+            <IconButton aria-label="Edit category">
+              <EditIcon/>
+            </IconButton>
+            <IconButton aria-label="Remove category" onClick={this.handleRemoval}>
+              <DeleteIcon/>
+            </IconButton>
+          </CardActions>
       </Card>
     )
   }
