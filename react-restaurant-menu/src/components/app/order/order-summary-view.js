5a047d6ecda98b67c908a1aa12fe6f4e0b86d3f4
rocioimpa <rocio.impaglione@gmail.com>
2019-11-08 00:26:45 -0300

    Removing unused variable

--- react-restaurant-menu/src/components/app/order/order-summary-view.js
+++ react-restaurant-menu/src/components/app/order/order-summary-view.js
@@ -7,7 +7,7 @@ export default class OrderSummaryView extends Component {
         <h4 className="title">{this.props.courseTitle}</h4>
         <ul>
           {this.props.courseItems.map(item => (
-            <li key={item.id} price={item.price} >{item.title}</li>
+            <li key={item.id} >{item.title}</li>
           ))}
         </ul>
       </div>
