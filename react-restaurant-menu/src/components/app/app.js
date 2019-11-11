54122725b287151eedefe62e6f306112f6baeee3
luismj <kainblue@gmail.com>
2019-11-11 09:01:35 -0300

    Modificaciones en front

--- react-restaurant-menu/src/components/app/app.js
+++ react-restaurant-menu/src/components/app/app.js
@@ -1,10 +1,15 @@
 import React from 'react';
 //import './App.css';
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, NavLink, Switch } from "react-router-dom";
+import CommentComponent from './client/comments.component';
+import DailyMenuComponent from './client/dailymenu.component';
+import DishesComponent from './client/dishes.component';
+import OrderComponent from './client/order.component';
 import { routes } from "../../config/routes/index.js";
+import 'materialize-css/dist/css/materialize.min.css';
 
 function App() {
-  return (
+  /*return (
     <div className="container no-padding-xs pb-4 flex-grow-1" id="page-wrap">
       
       <Switch>
@@ -12,6 +17,34 @@ function App() {
       </Switch>
       
     </div>
+    );*/
+    return(
+      <Router>
+        <nav className="nav-wrapper amber darken-3">
+          
+          <ul className="right hide-on-med-and-down">
+            <li>
+              <NavLink activeClassName="active" to="/">Daily Menu</NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName="active" to="/dishes">Dishes</NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName="active" to="/orders">Your Order</NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName="active" to="/comments">Comments</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/" exact component={ DailyMenuComponent }></Route>
+          <Route path="/dishes" exact component={ DishesComponent }></Route>
+          <Route path="/comments" exact component={ CommentComponent }></Route>
+          <Route path="/orders" exact component={ OrderComponent }></Route>
+        </Switch>
+      </Router>
+
     );
 }
 
